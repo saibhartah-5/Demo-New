7525d3603ce6ce71b175f42122114cf1f8e8c308
saibharath-4 <saibharath.dachepalli4@gmail.com>
2016-06-17 10:07:22 +0530

    Added two lines

--- src/one/HelloWorld.java
+++ src/one/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		System.out.println("world!");
+		System.out.println("hello world");
+		System.out.println("This is ramarao");
 
 	}
 
